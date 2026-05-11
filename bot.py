24e1fe54ba8316f0744d0c829c389e513f47546d
axelboltnobanpls-cmyk <axelboltnobanpls@gmail.com>
2026-05-11 12:06:03 +0700

    Update bot.py
    Оставил как было

--- bot.py
+++ bot.py
@@ -12,8 +12,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.enums import ParseMode
 
 # ======================== НАСТРОЙКИ ========================
-python
-   BOT_TOKEN = os.environ.get("BOT_TOKEN", "ВАШ_ТОКЕН")           # Токен от @BotFather
+BOT_TOKEN = "8375269633:AAElLiElz8WvjdR_OxeCR6mBJZo6kzGK8xM"           # Токен от @BotFather
 CHANNEL_USERNAME = "@growagarden_arferno"       # Юзернейм канала (С @, БЕЗ t.me/)
 KEYS_FILE = "keys.json"
 DATABASE_FILE = "users.db"
